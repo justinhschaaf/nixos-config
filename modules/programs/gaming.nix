4220c5a8bbf7ee6bd443a4eb266308fe4394485b
Justin Schaaf <justinhschaaf@outlook.com>
2024-04-23 02:54:20 -0700

    fix: swap Steam Flatpak for nixpkgs version

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -10,18 +10,25 @@
         programs.steam.dedicatedServer.openFirewall = true;
         programs.steam.remotePlay.openFirewall = true;
 
+        # System packages. Installing Steam here instead of as a Flatpak because
+        # the Flatpak updates drivers based on the system drivers. Upon running
+        # a system update, if the system installs new drivers, Flatpak will only
+        # see the currently installed drivers and install a version to match
+        # those, thus always being out of date.
         environment.systemPackages = with pkgs; [
             davinci-resolve
             ffmpeg
             gamemode
             gamescope
-            nvidia-system-monitor-qt
-        ];
+            steam
+        # Only install Nvidia system monitor if Nvidia is enabled
+        ] ++ lib.optional config.js.desktop.nvidia.enable pkgs.nvidia-system-monitor-qt;
 
         # Flatpak config
+        # According to `flatpak info --show-extensions fr.handbrake.ghb`,
+        # Handbrake doesn't depend on Flatpak Nvidia drivers, so it can be here
         services.flatpak.packages = [
             "flathub:app/com.mojang.Minecraft//stable"
-            "flathub:app/com.valvesoftware.Steam//stable"
             "flathub:app/fr.handbrake.ghb//stable"
             "flathub:app/net.davidotek.pupgui2//stable"
         ];
