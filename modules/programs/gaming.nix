ef76cd3e7e4bde08b23d2c6207c3b22fa114cac0
Justin Schaaf <justinhschaaf@outlook.com>
2024-04-24 01:30:28 -0700

    feat: smater install for Steam, Gamescope, and Gamemode
    Install Steam, Gamescope, and Gamemode using the module options instead of declaring them in environment.systemPackages

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -6,21 +6,31 @@
 
     config = lib.mkIf config.js.programs.gaming.enable {
 
-        # Let Steam play nice with the firewall
-        programs.steam.dedicatedServer.openFirewall = true;
-        programs.steam.remotePlay.openFirewall = true;
-
-        # System packages. Installing Steam here instead of as a Flatpak because
+        # Steam settings. Installing Steam here instead of as a Flatpak because
         # the Flatpak updates drivers based on the system drivers. Upon running
-        # a system update, if the system installs new drivers, Flatpak will only
-        # see the currently installed drivers and install a version to match
-        # those, thus always being out of date.
+        # a system update, if the system installs new drivers, Flatpak will 
+        # only see the currently installed drivers and install a version to
+        # match those, thus always being out of date.
+        programs.steam = {
+        
+            enable = true;
+
+            # Let Steam use Gamescope by default. This also installs it.
+            gamescopeSession.enable = true;
+            
+            # Let Steam play nice with the firewall
+            dedicatedServer.openFirewall = true;
+            remotePlay.openFirewall = true;
+            
+        };
+
+        # Enable gamemode
+        programs.gamemode.enable = true;
+
+        # Other system packages
         environment.systemPackages = with pkgs; [
             davinci-resolve
             ffmpeg
-            gamemode
-            gamescope
-            steam
         ];
 
         # Flatpak config
