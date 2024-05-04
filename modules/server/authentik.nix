fb73f16ed7f2564924ca4154e7478b508115ed35
Justin Schaaf <justin@justinschaaf.com>
2024-05-04 04:22:57 -0700

    feat: configure Caddy reverse proxy for server modules

--- modules/server/authentik.nix
+++ modules/server/authentik.nix
@@ -6,6 +6,7 @@
 
     options = {
         js.server.authentik.enable = lib.mkEnableOption "Authentik, the authentication glue you need";
+        js.server.authentik.hostName = lib.mkOption { type = lib.types.str };
     };
 
     config = lib.mkIf config.js.server.authentik.enable {
@@ -26,6 +27,18 @@
             };
         };
 
+        services.caddy = lib.mkIf config.js.server.caddy.enable {
+            virtualHosts."${config.js.server.authentik.hostName}" = {
+                # https://docs.goauthentik.io/docs/installation/reverse-proxy
+                # Headers should already be set, see https://caddyserver.com/docs/caddyfile/directives/reverse_proxy#defaults
+                extraConfig = ''
+                    handle {
+                        reverse_proxy 127.0.0.1:9443
+                    }
+                '';
+            };
+        };
+    
     };
 
 }
--- modules/server/seafile.nix
+++ modules/server/seafile.nix
@@ -2,6 +2,7 @@
 
     options = {
         js.server.seafile.enable = lib.mkEnableOption "Seafile, the Next-generation Open Source Cloud Storage";
+        js.server.seafile.hostName = lib.mkOption { type = lib.types.str };
     };
 
     config = lib.mkIf config.js.server.seafile.enable {
@@ -13,6 +14,7 @@
             # that will be changed by the time this is ever accessible to a 
             # public network
             initialAdminPassword = "Spaghett!0s"; # this will be changed when deployed so don't try it
+            ccnetSettings.General.SERVICE_URL = "https://${config.js.server.seafile.hostName}"
             seafileSettings = {
                 quota.default = 5; # in gb
                 library_trash.espire_days = 60;
@@ -20,6 +22,35 @@
             };
         };
 
+        services.caddy = lib.mkIf config.js.server.caddy.enable {
+            virtualHosts."${config.js.server.seafile.hostName}" = {
+                # https://forum.seafile.com/t/notification-server-behind-caddy/19326
+                # https://forum.seafile.com/t/caddy-reverse-proxy-for-seafile/19525
+                extraConfig = ''
+                    handle_path /seafile/notification/* {
+                        @websockets {
+                            header Connection *Upgrade*
+                            header Upgrade    websocket
+                        }
+                        reverse_proxy @websockets 127.0.0.1:8083
+                    }
+                    handle_path /seafhttp* {
+                        reverse_proxy 127.0.0.1:8082
+                    }
+                    handle_path /seafdav* {
+                        reverse_proxy 127.0.0.1:8080
+                    }
+                    handle_path /media* {
+                        root * /var/lib/seafile/seahub
+                        file_server
+                    }
+                    handle {
+                        reverse_proxy 127.0.0.1:8000
+                    }
+                '';
+            };
+        };
+    
     };
 
 }
