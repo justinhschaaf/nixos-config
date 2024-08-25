9a5cb6be2f8d1b185e309f6037884ea4238c5e4a
Justin Schaaf <justin@justinschaaf.com>
2024-08-25 05:31:31 -0700

    fix: unable to install system due to /boot mount flags
    Remove mount flags for /boot since trying to set its mount flags during install when it doesn't exist prevents install

--- modules/default.nix
+++ modules/default.nix
@@ -49,8 +49,8 @@
     # Enable nodev and nosuid on most file systems
     # Not enabling noexec on most since I'm a developer, I need to do dev stuff!
     # noexec was enabled for /root, /home, /srv, and /var/log but Nix complained those aren't actual mounts
+    # disabling /boot options because Nix complains about it duuring install
     fileSystems."/".options = [ "nodev" "nosuid" ];
-    fileSystems."/boot".options = [ "nodev" "noexec" "nosuid" ];
 
     # Enable networking, usuable with nmtui
     # Also enable privacy tweaks to hide MAC address https://privsec.dev/posts/linux/desktop-linux-hardening/#privacy-tweaks
