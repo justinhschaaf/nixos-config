7353bfbb1a4bada566fbc22f4ce32a8a2ab1c1d7
Justin Schaaf <justin@justinschaaf.com>
2024-09-28 22:31:51 -0700

    fix: disable duplicate command complete notification

--- hmmodules/terminal.nix
+++ hmmodules/terminal.nix
@@ -55,7 +55,6 @@
 
                 # Notifications
                 enable_audio_bell = "no";
-                notify_on_cmd_finish = "invisible 10.0 notify";
 
                 # https://github.com/kovidgoyal/kitty/issues/713
                 # https://sw.kovidgoyal.net/kitty/conf/#opt-kitty.term
