cb4fee951b0eb26a967858342cb2fa5f35bb3a84
Justin Schaaf <justin@justinschaaf.com>
2024-11-17 16:24:07 -0800

    feat: start 3d printer system config

---
+++ systems/ender.nix
@@ -0,0 +1,35 @@
+{ inputs, lib, config, pkgs, ... }: {
+
+    # TODO disko config
+    #imports = [ ./server-hardware.nix ];
+
+    js.sops.enable = true;
+    js.server = {
+
+        enable = true;
+        openFirewall = true; # openFirewall != port forwarded, only accessible to internal network
+
+        # Remote access
+        ssh.enable = true;
+        tarpits.enable = true;
+
+        # Monitoring
+        prometheus.exporters.node.enable = true;
+        prometheus.exporters.comin.enable = true;
+        loki.agents.promtail.enable = true;
+
+    };
+
+    # Set system name
+    networking.hostName = "pastificio";
+
+    # Setup static IP and default gateway
+    # TODO determine correct interface when deploying
+    networking.defaultGateway = "10.10.22.1";
+    networking.interfaces.enp2s0.ipv4.addresses = [{
+        address = "10.10.22.19";
+        prefixLength = 24;
+    }];
+
+}
+
