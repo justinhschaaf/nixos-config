6c8a73518b8c3e2c84966bbab1407612f9ef2aae
Justin Schaaf <justin@justinschaaf.com>
2025-02-28 23:40:53 -0800

    feat: add bitwarden apps

--- hmusers/justinhs.nix
+++ hmusers/justinhs.nix
@@ -6,16 +6,24 @@
 
     # User-specific packages. I usually like having them at the system level.
     home.packages = with pkgs; [
+        # bitwarden password/secrets manager
+        bitwarden-cli
+        bitwarden-desktop
+        bws
+
+        # communication
+        element-desktop
+        signal-desktop
+        webcord
+
+        # other
         collision
-        #element-desktop
         inkscape
         libreoffice
         marktext
         obs-studio
         pinta
-        signal-desktop
         tenacity
-        webcord
     ];
 
     # Flatpak config
@@ -49,6 +57,10 @@
     home.file = lib.mkIf ("${osConfig.system.name}" == "bucatini" && osConfig.js.programs.gaming.enable) {
         "the_shit/Games/Steam/steamapps/compatdata".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.steam/steam/steamapps/compatdata";
     };
+
+    # Configure SSH to communicate with Bitwarden for authentication
+    # https://bitwarden.com/help/ssh-agent/#configure-bitwarden-ssh-agent
+    home.sessionVariables.SSH_AUTH_SOCK = "${config.home.homeDirectory}/.bitwarden-ssh-agent.sock";
 
     ######## Stuff that shouldn't be touched is below this line ########
 
