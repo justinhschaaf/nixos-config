5f41b1ee6ccc873a87b08eb52a64313348754b34
Justin Schaaf <justin@justinschaaf.com>
2025-03-22 01:20:31 -0700

    fix: use default hyprland animation config
    hopefully there is a default set. fixes config errors in the latest version where <animations:animation> does not exist

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -174,20 +174,6 @@
                     };
                 };
 
-                # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-                animations = {
-                    enabled = true;
-                    bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-                    animation = [
-                        "windows, 1, 7, myBezier"
-                        "windowsOut, 1, 7, default, popin 80%"
-                        "border, 1, 10, default"
-                        "borderangle, 1, 8, default"
-                        "fade, 1, 7, default"
-                        "workspaces, 1, 6, default"
-                    ];
-                };
-
                 # Nvidia performance
                 # https://wiki.hyprland.org/Nvidia/
                 cursor.no_hardware_cursors = true;
