78cb6eae06f55d138959a3b1008091ad2d836f60
Justin Schaaf <justin@justinschaaf.com>
2025-12-05 19:34:43 -0800

    Revert "fix: re-enable olive video editor"
    This reverts commit c985f211a7763400f845101b350008f24795acb9.

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -33,7 +33,7 @@
             handbrake
             #minecraft # just use modrinth lol
             modrinth-app
-            olive-editor
+            #olive-editor # https://github.com/NixOS/nixpkgs/issues/452183
             protonup-qt
         ];
 
