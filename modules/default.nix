28b9d1781c74883c5c05f6d7554ed4a8d7e0f6ce
justinhschaaf <justin@justinschaaf.com>
2026-04-25 17:57:50 -0700

    feat: kernel hardening

--- modules/default.nix
+++ modules/default.nix
@@ -70,6 +70,87 @@
     # Use Zen kernel instead https://github.com/zen-kernel/zen-kernel/wiki/Detailed-Feature-List
     boot.kernelPackages = pkgs.linuxPackages_zen;
 
+    # Blacklist unnecessary kernel modules
+    # https://wiki.nixos.org/wiki/NixOS_Hardening#Module_blacklist
+    boot.blacklistedKernelModules = [
+        # Obscure network protocols
+        "ax25"
+        "netrom"
+        "rose"
+
+        # Old or rare or insufficiently audited filesystems
+        "adfs"
+        "affs"
+        "bfs"
+        "befs"
+        "cramfs"
+        "efs"
+        "erofs"
+        "exofs"
+        "freevxfs"
+        "f2fs"
+        "hfs"
+        "hpfs"
+        "jfs"
+        "minix"
+        "nilfs2"
+        "omfs"
+        "qnx4"
+        "qnx6"
+        "sysv"
+        "ufs"
+    ];
+
+    # harden kernel params https://wiki.nixos.org/wiki/NixOS_Hardening#Kernel_parameters
+    boot.kernelParams = [
+        # Don't merge slabs
+        "slab_nomerge"
+
+        # Overwrite free'd pages
+        "page_poison=1"
+
+        # Enable page allocator randomization
+        "page_alloc.shuffle=1"
+
+        # Disable debugfs
+        "debugfs=off"
+    ];
+
+    # Secure sysctl params https://wiki.nixos.org/wiki/NixOS_Hardening#Sysctl_parameters
+
+    # Hide kptrs even for processes with CAP_SYSLOG
+    boot.kernel.sysctl."kernel.kptr_restrict" = "2";
+
+    # Disable bpf() JIT (to eliminate spray attacks)
+    boot.kernel.sysctl."net.core.bpf_jit_enable" = false;
+
+    # Disable ftrace debugging
+    boot.kernel.sysctl."kernel.ftrace_enabled" = false;
+
+    # Enable strict reverse path filtering (that is, do not attempt to route
+    # packets that "obviously" do not belong to the iface's network; dropped
+    # packets are logged as martians).
+    boot.kernel.sysctl."net.ipv4.conf.all.log_martians" = true;
+    boot.kernel.sysctl."net.ipv4.conf.all.rp_filter" = "1";
+    boot.kernel.sysctl."net.ipv4.conf.default.log_martians" = true;
+    boot.kernel.sysctl."net.ipv4.conf.default.rp_filter" = "1";
+
+    # Ignore broadcast ICMP (mitigate SMURF)
+    boot.kernel.sysctl."net.ipv4.icmp_echo_ignore_broadcasts" = true;
+
+    # Ignore incoming ICMP redirects (note: default is needed to ensure that the
+    # setting is applied to interfaces added after the sysctls are set)
+    boot.kernel.sysctl."net.ipv4.conf.all.accept_redirects" = false;
+    boot.kernel.sysctl."net.ipv4.conf.all.secure_redirects" = false;
+    boot.kernel.sysctl."net.ipv4.conf.default.accept_redirects" = false;
+    boot.kernel.sysctl."net.ipv4.conf.default.secure_redirects" = false;
+    boot.kernel.sysctl."net.ipv6.conf.all.accept_redirects" = false;
+    boot.kernel.sysctl."net.ipv6.conf.default.accept_redirects" = false;
+
+    # Ignore outgoing ICMP redirects (this is ipv4 only)
+    boot.kernel.sysctl."net.ipv4.conf.all.send_redirects" = false;
+    boot.kernel.sysctl."net.ipv4.conf.default.send_redirects" = false;
+
     # Enable nodev and nosuid on most file systems
     # Not enabling noexec on most since I'm a developer, I need to do dev stuff!
     # noexec was enabled for /root, /home, /srv, and /var/log but Nix complained those aren't actual mounts
@@ -85,6 +166,10 @@
     # https://man.archlinux.org/man/tmpfiles.d.5
     # https://www.baeldung.com/linux/systemd-tmpfiles-configure-temporary-files
     systemd.tmpfiles.rules = [ "D! %T 1777 root root" ];
+
+    # Enable AppArmor
+    security.apparmor.enable = true;
+    security.apparmor.killUnconfinedConfinables = true;
 
     # Replace sudo with sudo-rs and only let wheel use it
     security.sudo.enable = false;
