29767b13934f5683c474c9bbfe1e52a0f10bcd15
justinhschaaf <justin@justinschaaf.com>
2026-07-18 13:41:50 -0700

    chore: update caddy hash

--- modules/server/services/caddy.nix
+++ modules/server/services/caddy.nix
@@ -20,7 +20,7 @@
         services.caddy.enable = true;
         services.caddy.package = pkgs.caddy.withPlugins {
             plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-            hash = "sha256-8yZDrejNKsaUnUaTUFYbarWNmxafqp2z2rWo+XRsxV8=";
+            hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
         };
 
         # Add Cloudflare for DNS challenges
