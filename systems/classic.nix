3e467bf4e860895cbaa52df345054e83024454f1
justinhschaaf <justin@justinschaaf.com>
2026-07-30 01:03:15 -0700

    feat: replace classic system hdd with nvme ssd
    gotta redo the btrfs subvolume too

--- systems/classic.nix
+++ systems/classic.nix
@@ -2,7 +2,7 @@
 
     # Partition disk
     js.disks.enable = true;
-    js.disks.device = "/dev/sda";
+    js.disks.device = "/dev/nvme0n1";
     js.disks.swap.enable = true;
 
     # Create shared dir for game roms
@@ -19,16 +19,16 @@
     # Mount shared dir for game roms
     # This is a manually-created btrfs subvolume
     # btrfs create /media/SHARE
-    fileSystems."/media/SHARE" = {
-        device = "/dev/sda3";
-        fsType = "btrfs";
-        options = [
-            "subvol=media/SHARE"
-            "compress=zstd"
-            "noatime"
-            "noacl"
-        ];
-    };
+    #fileSystems."/media/SHARE" = {
+    #    device = "/dev/nvme0n1p3";
+    #    fsType = "btrfs";
+    #    options = [
+    #        "subvol=media/SHARE"
+    #        "compress=zstd"
+    #        "noatime"
+    #        "noacl"
+    #    ];
+    #};
 
     # Enable Intel CPU support
     js.hardware.intel.cpu.enable = true;
